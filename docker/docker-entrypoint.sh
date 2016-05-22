54ce1e7a5964db54d194471852396a5f0176efbf
LEGENDRE <legendre.gui@gmail.com>
2016-05-23 00:52:48 +0200

    move nginx to prod only

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -7,6 +7,6 @@ bundle check || bundle install
 
 # I'm not sure why but the server.pid isn't remove at the end of container, so
 # we try to remove it at each start of the server.
-rm tmp/pids/server.pid
+rm tmp/pids/server.pid > /dev/null 2> /dev/null
 
 bundle exec puma -C /myapp/config/puma.rb -p 3000
