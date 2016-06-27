d5fdc7a56b0ae78c0670a35cb06173a584d5e965
LEGENDRE <GuillaumeLegendre@users.noreply.github.com>
2016-06-27 15:56:59 +0200

    Update docker-clean.sh

--- docker/docker-clean.sh
+++ docker/docker-clean.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
 
 # Supprime les containers qui ont le status exited
+# add -v to remove volume with it?
 docker rm $(docker ps --filter status=exited -q 2>/dev/null) 2>/dev/null
 # Supprime les images obsolètes
 docker rmi $(docker images --filter dangling=true -q 2>/dev/null) 2>/dev/null
